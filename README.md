d977e5611fed07f2c6c0dbdf0948be9ee05dab1a
Kanda <ohhkanda@gmail.com>
2022-04-10 20:46:13 -0400

    created projects page with Bootstrap cards to link to project detail landing pages

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Django Portfolio/Resume/Blog website
 Working on Python skills while also setting up blog for Cloud Resume Challenge
+
+## Development
+Continious improvement on the web project/website should be done on this branch
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'projects',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["portfolio/templates/"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
