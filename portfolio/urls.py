3f9c18a619c962b0351f3a0efaf265571d3c05da
Kanda <ohhkanda@gmail.com>
2022-04-11 22:31:35 -0400

    resolved issue with urls page, made changes too soon

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -19,5 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('projects/', include('projects.urls')),
-    path('blog/', include('blog.urls'))
 ]
