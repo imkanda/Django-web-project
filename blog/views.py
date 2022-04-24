c5cce5bc28943cc18394356fb114564cc45a8237
kfalany <kyle@falany.info>
2022-04-24 17:48:55 -0400

    created blog urls, views, and templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from blog.models import Post
+
 
 # Create your views here.
+def blog_index(request):
+    posts = Post.objects.all().order_by('-created_on')
+    context = {
+        "posts": posts,
+    }
+    return render(request, "blog_index.html", context)
+
+def blog_category(request, category):
+    posts = Post.objects.filter(
+        categories__name__contains=category
+    ).order_by(
+        '-created_on'
+    )
+    context = {
+        "category": category,
+        "posts": posts,
+    }
+    return render(request, "blog_category.html", context)
+
+def blog_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    context = {
+        "post": post,
+    }
+
+    return render(request, "blog_detail.html", context)
